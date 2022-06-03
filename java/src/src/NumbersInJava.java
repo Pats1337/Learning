f067dc1be0f436cbde27cf02d1e0c7c4c4db27c3
Alex Terentev <Ater1337@yandex.ru>
2022-06-03 16:26:07 +0300

    Working with numbers and strings in Java

---
+++ java/src/src/NumbersInJava.java
@@ -0,0 +1,21 @@
+public class NumbersInJava {
+    public static void main(String[] args) {
+        /*
+        System.out.println(2+2);
+        System.out.println("2+2");
+        System.out.println(2+"2");
+        System.out.println(Integer.valueOf("2")+2);
+        System.out.println(String.valueOf(2)+"2");
+        */
+        showMath(3,5);
+        showMath(1000,-15);
+    }
+
+    private static void showMath(Integer i, int i1) {
+        System.out.println(i + i1);
+        System.out.println(i - i1);
+        System.out.println(i1 - i);
+        System.out.println(i * i1);
+        System.out.println(i / i1);
+    }
+}
