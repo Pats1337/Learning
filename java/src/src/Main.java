55d69c6ff10c27eb177a649c00db800e5132f9ea
Alex Terentev <Ater1337@yandex.ru>
2022-06-03 13:27:15 +0300

    Test methods and polymorph

---
+++ java/src/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        print("That String");
+        print(21);
+    }
+
+    private static void print(int num) {
+        System.out.println(num);
+    }
+    private static void print(String text) {
+        System.out.println(text);
+    }
+}
