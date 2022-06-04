01500e20b3897d85c5ad686174847d86d45f0845
Alex Terentev <Ater1337@yandex.ru>
2022-06-04 11:27:28 +0300

    Working with variables and functions in Java

--- java/src/src/SomeFuncInJava.java
+++ java/src/src/SomeFuncInJava.java
@@ -1,7 +1,10 @@
 public class SomeFuncInJava {
-
+    /* Classwork
     public static void main(String[] args) {
-        print(multiply(sum(3, 4), difference(7, 5)));
+        int sum1 = sum(3, 4);
+        int difference1 = difference(7, 5);
+        int multiplication1 = multiply(sum1, difference1);
+        print(multiplication1);
         print(multiply(sum(40, 60), difference(300, 270)));
     }
 
@@ -19,5 +22,23 @@ public class SomeFuncInJava {
 
     private static void print(int i) {
         System.out.println(i);
+    }
+    */
+    //Homework
+
+    public static void main(String[] args) {
+        String string1 = "- Hi, I'm Alex Terentev!\n";
+        String string2 = "  Nice to meet you.";
+        String string3 = sumString(string1, string2);
+        print(string3);
+
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
+
+    private static String sumString(String s1, String s2) {
+        return s1 + s2;
     }
 }
