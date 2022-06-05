cb5aa521e91439d03f9858705a48d18105a72c1d
Alex Terentev <Ater1337@yandex.ru>
2022-06-05 22:31:22 +0300

    Primary and re-initialization of a variable in Java

---
+++ java/src/src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+        final String characterName = "Maddy";
+        String typeOfFood = "Bull steak";
+        printMsg(typeOfFood, characterName);
+        typeOfFood = "Chicken salad";
+        printMsg(typeOfFood, characterName);
+        typeOfFood = "Cheese scones";
+        printMsg(typeOfFood, characterName);
+        typeOfFood = "Pickled mushrooms";
+        printMsg(typeOfFood, characterName);
+        typeOfFood = "Herring under a fur coat";
+        printMsg(typeOfFood, characterName);
+        typeOfFood = "Grandma's cake";
+        printMsg(typeOfFood, characterName);
+
+    }
+
+    private static void printMsg(String typeOfFood, String characterName) {
+        System.out.println(characterName + " ate the " + typeOfFood + " and hi is happy! <3");
+    }
+
+}
