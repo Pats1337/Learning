739957c8ca9d0b92a70921699acfa8e6bb34ac4b
Alex Terentev <Ater1337@yandex.ru>
2022-06-07 15:29:41 +0300

    Completed Exercise 2 of the homework for lesson 10

--- java/src/src/HomeworkForLesson10.java
+++ java/src/src/HomeworkForLesson10.java
@@ -1,6 +1,6 @@
 public class HomeworkForLesson10 {
 
-
+/* exercise 1
     public static void main(String[] args) {
         print(true, true);
         print(true, false);
@@ -15,6 +15,24 @@ public class HomeworkForLesson10 {
             System.out.println("I don't have any money, but I have some good friends. Let's invite him to the bar, because it's Friday night!");
         } else {
             System.out.println("It's not Friday, it's not time to drink, it's time to work!");
+        }
+    }
+} */
+    // exercise 2
+    public static void main(String[] args) {
+       print(3.20f, 5.75f, 15f);
+       print(3.20f, 5.75f, 4f);
+       print(3.20f, 5.75f, 5.75f);
+       print(3.20f, 5.75f, 1f);
+    }
+
+    private static void print(float beerPrice, float wiskeyPrice, float myBudget) {
+        if (myBudget >= wiskeyPrice) {
+            System.out.println("It's probably a great day for whiskey. Bartender, one whiskey, please!");
+        } else if (myBudget >= beerPrice) {
+            System.out.println("Eh, not enough whiskey today, so I'll have a beer. Buddy, pour me a mug of Guiness. ");
+        } else {
+            System.out.println("Unfortunately, we don't have any money left for booze. I'm going home. ");
         }
     }
 }
