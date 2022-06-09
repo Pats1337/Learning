c670cf1bb0c8a538c053509923d1eea514ca7854
Alex Terentev <Ater1337@yandex.ru>
2022-06-09 10:32:50 +0300

    Did exercise number 2

--- java/src/src/HomeworkForLesson11.java
+++ java/src/src/HomeworkForLesson11.java
@@ -1,5 +1,5 @@
 public class HomeworkForLesson11 {
-    // Ex.1
+    /* Ex.1
     public static void main(String[] args) {
         print(checkTriangle(3, 4, 5));
         print(checkTriangle(5, 1, 5));
@@ -17,4 +17,31 @@ public class HomeworkForLesson11 {
         }
     }
 }
+*/
+    // Ex.2
+    public static void main(String[] args) {
+        print(checkMin(1, 2, 3, 4));
+        print(checkMin(2, -1, 3, 4));
+        print(checkMin(2, -1, -3, 4));
+        print(checkMin(2, -1, 3, -904));
+        print(checkMin(-1, -2, -1, 4));
+    }
 
+    private static int checkMin(int a, int b, int c, int d) {
+        int x;
+        if(a <= b & a <= c & a <= d){
+            x = a;
+        } else if (b <= a & b <= c & b <= d){
+            x = b;
+        } else if (c <= a & c <= b & c <= d){
+            x = c;
+        } else {
+            x = d;
+        }
+        return x;
+    }
+
+    private static void print(int x){
+        System.out.println("The lowest number received: " + x);
+    }
+}
