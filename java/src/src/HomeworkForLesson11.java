795ba9215e933c5633fe677f0fb63c7103e04b4b
Alex Terentev <Ater1337@yandex.ru>
2022-06-09 11:03:28 +0300

    Did exercise number 3

--- java/src/src/HomeworkForLesson11.java
+++ java/src/src/HomeworkForLesson11.java
@@ -17,7 +17,7 @@ public class HomeworkForLesson11 {
         }
     }
 }
-*/
+
     // Ex.2
     public static void main(String[] args) {
         print(checkMin(1, 2, 3, 4));
@@ -45,3 +45,34 @@ public class HomeworkForLesson11 {
         System.out.println("The lowest number received: " + x);
     }
 }
+*/
+    // Ex.3
+    public static void main(String[] args) {
+        print(checkDay(true, true));
+        print(checkDay(true, false));
+        print(checkDay(false, true));
+        print(checkDay(false, false));
+    }
+
+    private static void print(int x) {
+        if (x == 1){
+            System.out.println("Today is a day off and a holiday, you can sleep as much as you want.");
+        } else if (x == 2){
+            System.out.println("Today is only a day off, lots to do around the house, so we set the alarm for 11 a.m.");
+        } else {
+            System.out.println("Work day! Set the alarm for 6:00 a.m.");
+        }
+    }
+
+    private static int checkDay(boolean weekend, boolean holiday) {
+        int x;
+        if ((weekend & holiday) || (!weekend & holiday)){
+            x = 1; // Today is a day off and a holiday, you can sleep as much as you want.
+        } else if (weekend & !holiday){
+            x = 2; // Today is only a day off, lots to do around the house, so we set the alarm for 11 a.m.
+        } else {
+            x = 3; // Work day! Set the alarm for 6:00 a.m.
+        }
+        return x;
+    }
+    }
