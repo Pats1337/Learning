a0fbd1c751c6de9bbf14269fc801580652cd1b74
Alex Terentev <Ater1337@yandex.ru>
2022-06-15 18:03:26 +0300

    Did exercise number 9

--- java/src/src/HomeworkForLesson11.java
+++ java/src/src/HomeworkForLesson11.java
@@ -260,7 +260,7 @@ public class HomeworkForLesson11 {
             return x;
         }
         }
-        */
+
     // Ex. 8
     private static int count;
     public static void main(String[] args) {
@@ -291,3 +291,38 @@ public class HomeworkForLesson11 {
         return x;
     }
     }
+
+     */
+    // Ex.9
+    public static void main(String[] args) {
+        print(checkCondition(-10));
+        print(checkCondition(0));
+        print(checkCondition(2));
+        print(checkCondition(100));
+        print(checkCondition(99));
+        print(checkCondition(121));
+    }
+
+    private static void print(int x) {
+        if (x == -1) {
+            System.out.println("Ice");
+        } else if (x == 0){
+            System.out.println("Water");
+        } else {
+            System.out.println("Gas");
+        }
+    }
+
+    private static int checkCondition(int temp) {
+        int x;
+        if (temp <= 0){
+            x = -1;
+        } else if (temp < 100){
+            x = 0;
+        } else {
+            x = 1;
+        }
+        return x;
+    }
+
+}
