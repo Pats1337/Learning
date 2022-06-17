95e706df9a436827468c0479de1da981a3e981cf
Alex Terentev <Ater1337@yandex.ru>
2022-06-17 12:45:36 +0300

    Did exercise number 10

--- java/src/src/HomeworkForLesson11.java
+++ java/src/src/HomeworkForLesson11.java
@@ -292,7 +292,7 @@ public class HomeworkForLesson11 {
     }
     }
 
-     */
+
     // Ex.9
     public static void main(String[] args) {
         print(checkCondition(-10));
@@ -326,3 +326,25 @@ public class HomeworkForLesson11 {
     }
 
 }
+
+     */
+    // Ex.10
+    public static void main(String[] args) {
+        print(checkLGBT("male", "female"));
+        print(checkLGBT("female", "female"));
+        print(checkLGBT("male", "male"));
+        print(checkLGBT("female", "male"));
+    }
+
+    private static void print(boolean checkLGBT) {
+        if (checkLGBT){
+            System.out.println("Live happily!");
+        }else {
+            System.out.println("Sorry, same-sex marriage is illegal in our country");
+        }
+    }
+
+    private static boolean checkLGBT(String x, String y) {
+        return !x.equals(y);
+    }
+    }
