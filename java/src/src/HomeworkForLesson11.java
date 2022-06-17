969934b5201d3792aadf42632babf77ce18ba0eb
Alex Terentev <Ater1337@yandex.ru>
2022-06-17 17:11:21 +0300

    Did exercise number 14

--- java/src/src/HomeworkForLesson11.java
+++ java/src/src/HomeworkForLesson11.java
@@ -388,7 +388,7 @@ public class HomeworkForLesson11 {
     }
 }
 
-     */
+
     //Ex.13
     public static void main(String[] args) {
         print(avgScore(4f, 5f, 5f, 4f, 4f));
@@ -428,5 +428,51 @@ public class HomeworkForLesson11 {
             avgScore = 5;
         }
         return avgScore;
+    }
+    }
+
+     */
+    //Ex.14
+    public static String name;
+    public static void main(String[] args) {
+        name = "Ivan Ivanov";
+        print(checkIvanIvanov(name));
+        name = "Penis Detrov";
+        print(checkIvanIvanov(name));
+        name = "Alex Ivanov";
+        print(checkIvanIvanov(name));
+        name = "Ivan Sazonov";
+        print(checkIvanIvanov(name));
+    }
+
+    private static void print(int nameStatus) {
+        switch(nameStatus){
+            case 0:
+                System.out.println("Error! Empty name.");
+                break;
+            case 1:
+                System.out.println("Sorry, " + name + ", access denied.");
+                break;
+            case 2:
+                System.out.println("C-C-C-COMBO, " + name + ", access denied.");
+                break;
+            case 3:
+                System.out.println("Welcome, " + name + ".");
+                break;
+        }
+    }
+
+    private static int checkIvanIvanov(String name) {
+        int nameStatus;
+        if (name.isEmpty()){
+            nameStatus = 0;
+        } else if (name.startsWith("Ivan") & name.endsWith("Ivanov")){
+            nameStatus = 2;
+        } else if (name.startsWith("Ivan") || name.endsWith("Ivanov")){
+            nameStatus = 1;
+        } else {
+            nameStatus = 3;
+        }
+        return nameStatus;
     }
     }
