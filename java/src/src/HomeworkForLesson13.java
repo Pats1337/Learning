3ae4fe1c405711f1e8a953aa80dba72dff935dd3
Alex Terentev <Ater1337@yandex.ru>
2022-06-20 14:30:23 +0300

    Did exercise number 5 (lesson 13)

--- java/src/src/HomeworkForLesson13.java
+++ java/src/src/HomeworkForLesson13.java
@@ -63,8 +63,6 @@ public class HomeworkForLesson13 {
         return negativeCounter;
     }
 
-     */
-
     //Ex.4
     public static int NUMBER;
 
@@ -91,5 +89,27 @@ public class HomeworkForLesson13 {
             }
         }
         return n;
+    }
+
+     */
+
+    //Ex.5
+    public static void main(String[] args) {
+        print(findEmptyStrings(new String[]{"Text1", "Text2", "Text3", ""}));
+        print(findEmptyStrings(new String[]{"", "", ""}));
+    }
+
+    private static void print(int emptyStrings) {
+        System.out.println("Количество пустых строк в массиве: " + emptyStrings);
+    }
+
+    private static int findEmptyStrings(String[] strings) {
+        int emptyStrings = 0;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].isEmpty()) {
+                emptyStrings++;
+            }
+        }
+        return emptyStrings;
     }
 }
