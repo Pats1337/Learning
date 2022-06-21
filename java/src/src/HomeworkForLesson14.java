17f42188ee8489f943b5dd87dadf31b347ee5efe
Alex Terentev <Ater1337@yandex.ru>
2022-06-21 11:05:28 +0300

    Did exercise number 4 (lesson 14)

--- java/src/src/HomeworkForLesson14.java
+++ java/src/src/HomeworkForLesson14.java
@@ -28,8 +28,6 @@ public class HomeworkForLesson14 {
         }
     }
 
-     */
-
     //Ex.3
     public static void main(String[] args) {
         checkFibonacci(-1);
@@ -59,5 +57,40 @@ public class HomeworkForLesson14 {
             }
             System.out.println();
         }
+    }
+
+     */
+    //Ex.4
+    public static void main(String[] args) {
+        print(findMinMax(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        print(findMinMax(new int[]{}));
+        print(findMinMax(new int[]{-1000, 10, 11, 1000, 121314, 0}));
+    }
+
+    private static void print(int diff) {
+        System.out.println("Разница максимального и минимального элементов массива: " + diff);
+    }
+
+    private static int findMinMax(int[] numbers) {
+        int min;
+        int max;
+        int diff;
+        if (numbers.length == 0) {
+            System.out.print("Error, array is empty! ");
+            diff = 0;
+        } else {
+            min = numbers[0];
+            max = numbers[0];
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] <= min) {
+                    min = numbers[i];
+                }
+                if (numbers[i] >= max) {
+                    max = numbers[i];
+                }
+            }
+            diff = max - min;
+        }
+        return diff;
     }
 }
