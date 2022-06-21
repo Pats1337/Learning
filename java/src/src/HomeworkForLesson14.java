18810990628d58b9a6a570d33fd3d4a5ad937f52
Alex Terentev <Ater1337@yandex.ru>
2022-06-21 10:19:40 +0300

    Did exercise number 2 (lesson 14)

--- java/src/src/HomeworkForLesson14.java
+++ java/src/src/HomeworkForLesson14.java
@@ -1,4 +1,6 @@
 public class HomeworkForLesson14 {
+    /*
+
     //Ex.1
     public static void main(String[] args) {
         checkMultiplicity(100);
@@ -11,4 +13,20 @@ public class HomeworkForLesson14 {
             }
         }
     }
+
+     */
+
+    //Ex.2
+    public static void main(String[] args) {
+        checkMultiplicity(5, 137);
+    }
+
+    private static void checkMultiplicity(int i, int i1) {
+        for (int n = 1; n <= i1; n++) {
+            if (n % i == 0) {
+                System.out.println(n);
+            }
+        }
+    }
+
 }
