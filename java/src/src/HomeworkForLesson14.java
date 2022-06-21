5703063af03aa54c11cd062b04a287269bb4b6e2
Alex Terentev <Ater1337@yandex.ru>
2022-06-21 12:46:24 +0300

    Did exercise number 6 (lesson 14)

--- java/src/src/HomeworkForLesson14.java
+++ java/src/src/HomeworkForLesson14.java
@@ -59,7 +59,6 @@ public class HomeworkForLesson14 {
         }
     }
 
-     */
     //Ex.4
     public static void main(String[] args) {
         print(findMinMax(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
@@ -93,4 +92,21 @@ public class HomeworkForLesson14 {
         }
         return diff;
     }
+
+     */
+    //Ex.6
+    public static void main(String[] args) {
+        mirrorArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        mirrorArray(new int[]{1});
+        mirrorArray(new int[]{});
+        mirrorArray(new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1});
+    }
+
+    private static void mirrorArray(int[] numbers) {
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
 }
+
