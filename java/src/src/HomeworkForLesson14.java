42f43001cdf20cc966deb6147395c0ead976e639
Alex Terentev <Ater1337@yandex.ru>
2022-06-21 10:37:22 +0300

    Did exercise number 3 (lesson 14)

--- java/src/src/HomeworkForLesson14.java
+++ java/src/src/HomeworkForLesson14.java
@@ -14,7 +14,6 @@ public class HomeworkForLesson14 {
         }
     }
 
-     */
 
     //Ex.2
     public static void main(String[] args) {
@@ -29,4 +28,36 @@ public class HomeworkForLesson14 {
         }
     }
 
+     */
+
+    //Ex.3
+    public static void main(String[] args) {
+        checkFibonacci(-1);
+        checkFibonacci(1);
+        checkFibonacci(2);
+        checkFibonacci(3);
+        checkFibonacci(20);
+    }
+
+    private static void checkFibonacci(long n) {
+        long i0 = 0;
+        long i1 = 1;
+        long i2;
+        if (n <= 0) {
+            System.out.println("Error, invalid value!");
+        } else if (n == 1) {
+            System.out.println(i0);
+        } else if (n == 2) {
+            System.out.println(i0 + " " + i1);
+        } else {
+            System.out.print(i0 + " " + i1 + " ");
+            for (long i = 3; i <= n; i++) {
+                i2 = i0 + i1;
+                System.out.print(i2 + " ");
+                i0 = i1;
+                i1 = i2;
+            }
+        }
+        System.out.println();
+    }
 }
