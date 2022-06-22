75604e4a875e9273363221567ec13561afa47515
Alex Terentev <Ater1337@yandex.ru>
2022-06-22 10:22:33 +0300

    Did exercise num.3 (lesson 15)

--- java/src/src/WhileAndInputToTheConsole.java
+++ java/src/src/WhileAndInputToTheConsole.java
@@ -20,8 +20,6 @@ public class WhileAndInputToTheConsole {
         }
     }
 
-     */
-
     //Ex.2
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -35,6 +33,29 @@ public class WhileAndInputToTheConsole {
                 System.out.println("Введенное число не делиться на 5 без остатка");
             }
             count++;
+        }
+    }
+
+     */
+
+    //Ex.3 (Credit bot)
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Добро пожаловать в банк!\nДля предоставления кредита необходимо уточнить некоторые данные.");
+        System.out.print("Сумма кредита: ");
+        int money = scanner.nextInt();
+        System.out.print("Заработная плата в месяц: ");
+        int salary = scanner.nextInt();
+        System.out.print("Срок кредита: ");
+        int time = scanner.nextInt();
+        if (money <= 150000 && salary >= 25000 && time <= 12){
+            System.out.println("Вам одобрен кредит на сумму " + money + " со сроком кредитования " + time + " месяцев.");
+        } else if (money > 150000){
+            System.out.println("Извините, запрашиваемая сумма превышает максимальную сумму выдачи.\nВ кредитовании ОТКАЗАНО!");
+        } else if (salary < 25000){
+            System.out.println("Извините, размер Вашей заработной платы меньше минимальной суммы.\nВ кредитовании ОТКАЗАНО!");
+        } else {
+            System.out.println("Извините, срок кредитования превышает максимальный лимит.\nВ кредитовании ОТКАЗАНО!");
         }
     }
 }
