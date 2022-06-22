d102e4ebda1b6b28373d1cdf6c4af19bc8160f16
Alex Terentev <Ater1337@yandex.ru>
2022-06-22 12:04:24 +0300

    Did exercise num.5 (lesson 15)

--- java/src/src/WhileAndInputToTheConsole.java
+++ java/src/src/WhileAndInputToTheConsole.java
@@ -57,7 +57,6 @@ public class WhileAndInputToTheConsole {
         }
     }
 
-     */
     //Ex.4 (Job offer bot)
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -75,6 +74,29 @@ public class WhileAndInputToTheConsole {
                 System.out.println("Мы Вам перезвоним.\nJob rejected");
             }
             break;
+        }
+    }
+
+     */
+    //Ex.5 (Checking the arithmetic progression)
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 2;
+        int diff0, diff1;
+        System.out.println("Введите числа образующие арифмитическую прогрессию: ");
+        int number = scanner.nextInt();
+        int number1 = scanner.nextInt();
+        diff0 = number1 - number;
+        int lastNum = number1;
+        while (count < 10) {
+            int number2 = scanner.nextInt();
+            diff1 = number2 - lastNum;
+            if (diff1 != diff0){
+                System.out.println("Введенные числа не являются арифмитической прогрессией!");
+                break;
+            }
+            lastNum = number2;
+            count++;
         }
     }
 }
