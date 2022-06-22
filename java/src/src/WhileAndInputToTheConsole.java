f8b36dfe19f87313434e1b13b98e54a836cb9ec1
Alex Terentev <Ater1337@yandex.ru>
2022-06-22 10:03:52 +0300

    Did exercise num.2 (lesson 15)

--- java/src/src/WhileAndInputToTheConsole.java
+++ java/src/src/WhileAndInputToTheConsole.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class WhileAndInputToTheConsole {
+
+    /*
+
     //Ex.1
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,6 +16,24 @@ public class WhileAndInputToTheConsole {
                 break;
             }
             System.out.println("Привет, " + name + "!");
+            count++;
+        }
+    }
+
+     */
+
+    //Ex.2
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        while (count < 20) {
+            System.out.print("Введите число: ");
+            int number = scanner.nextInt();
+            if (number % 5 == 0) {
+                System.out.println("Введенное число кратно 5");
+            } else {
+                System.out.println("Введенное число не делиться на 5 без остатка");
+            }
             count++;
         }
     }
