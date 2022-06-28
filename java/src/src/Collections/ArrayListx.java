e825474b0b6c036c5f59b740924ac293f305629f
Alex Terentev <Ater1337@yandex.ru>
2022-06-28 14:04:27 +0300

    Testing the List collection

---
+++ java/src/src/Collections/ArrayListx.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.*;
+
+public class ArrayListx {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        System.out.println(list.isEmpty());
+        list.add("first");
+        list.add("second");
+        for (String string : list) {
+            System.out.println(string);
+        }
+        list.remove("first");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("i " + i + " item " + list.get(i));
+        }
+    }
+}
