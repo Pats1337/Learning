1b7c51b90163f77f4b92eb0d020638c60ca7fbf0
Alex Terentev <Ater1337@yandex.ru>
2022-06-28 14:33:35 +0300

    Testing the List collection 3.0 (addAll method)

--- java/src/src/Collections/ArrayListx.java
+++ java/src/src/Collections/ArrayListx.java
@@ -5,22 +5,14 @@ import java.util.*;
 public class ArrayListx {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        System.out.println(list.isEmpty());
         list.add("first");
         list.add("second");
-        for (String string : list) {
-            System.out.println(string);
-        }
-        list.remove("first");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println("i " + i + " item " + list.get(i));
-        }
-        list.add(1, "third");
-        list.add("fourth");
-        System.out.println("added 2 items");
-        list.remove(list.size() - 1);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println("i " + i + " item " + list.get(i));
+        list.add("third");
+        List<String> big = new ArrayList<>();
+        big.add("big");
+        big.addAll(list);
+        for (String string : big){
+            System.out.println(string + " ");
         }
     }
 }
