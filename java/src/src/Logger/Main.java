afd2cbcfdd55ce896bff5dcd797e32ddba223096
Alex Terentev <Ater1337@yandex.ru>
2022-07-05 16:21:32 +0300

    Create synchronized logger

---
+++ java/src/src/Logger/Main.java
@@ -0,0 +1,30 @@
+package Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = new Logger(new ArrayList<>());
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            int finalI = i;
+            threads.add(new Thread(() -> logger.addLog("Header " + finalI, "body " + finalI)));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                logger.print();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 5000);
+    }
+}
